303b2ba6dcccc7382f14701cd1aafa94e5d4ad08
Lu Valcke <lfvalcke@gmail.com>
2021-07-20 15:52:30 -0500

    Avance filtro por directores

--- src/data.js
+++ src/data.js
@@ -1,9 +1,17 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+// const miyazaki = [data.films.filter( directors => (directors.director === "Hayao Miyazaki"))];
+
+// console.log(miyazaki);
+
+// export const movieFilter = (datos, director) => {
+//   return datos.films.filter(dataMovies => (dataMovies.director === director))
+// }
