708553b192e3bed3d608b94b7997b010facc3185
Lu Valcke <lfvalcke@gmail.com>
2021-07-26 11:49:55 -0500

    Maqueta y funcionalidad historia de usuario 3

--- src/data.js
+++ src/data.js
@@ -2,3 +2,11 @@
 export let moviesList = function (dataMovie, director){
     return dataMovie.filter(movies=>movies.director===director).map(film=>film.title);
 }
+
+export let dataSortedNew = function(dataMovie){
+    return dataMovie.sort((a, b) => (a.release_date > b.release_date) ? 1 : -1);
+}
+
+export let dataSortedOld = function(dataMovie){
+    return dataMovie.sort((a, b) => (b.release_date > a.release_date) ? 1 : -1);
+}
